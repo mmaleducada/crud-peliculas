e415f7cddf51a11242e7905197daf102c1ee9d19
Malena de Arriba <dearribamalena@gmail.com>
2023-04-26 20:04:08 -0300

    agregue al localstorage la opcion del usuario y cambie parte del maquetado en la pagina de detalles

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+//cuando cargue la pagina, lo primero que quiero es que lea el local storage para ver que tema quiere el usuario. Si no hay nada, poner el tema por defecto
+//JSON.parse realiza el cambio de un elemento en formato JSON y lo vuelve a lo que era anteriormente
+let temaConfigurado = JSON.parse(localStorage.getItem("colorTema")) || "light";
+cambiarTema(temaConfigurado);
+
+
 let btnDark = document.getElementById("btn-dark");
 let btnLight = document.getElementById("btn-light");
 
@@ -5,8 +11,11 @@ btnDark.addEventListener("click", () => cambiarTema("dark"));
 btnLight.addEventListener("click", () => cambiarTema("light"));
 
 function cambiarTema (color) {
-    // traemos la etiqueta html ya que utilizaremos el atributo data-base-theme de bootstrap
+    // traemos la etiqueta html ya que utilizaremos el atributo data-base-theme de bootstrap para cambiar el color del tema de la pagina
     // propiedad de la etiqueta html: setAttribute(atributo,valor)
     document.querySelector("html").setAttribute("data-bs-theme", color);
+    // guardamos el contenido de la variable color en el localstorage
+    // setItem guarda en el localstorage - lleva dos parámetros: key=palabra que nos recuerda que guardamos - valor que guardamos
+    localStorage.setItem("colorTema", JSON.stringify(color))
 
 }
