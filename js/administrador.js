2772813a54126166ad0cc8218715fa4ac8459fce
Malena de Arriba <dearribamalena@gmail.com>
2023-04-27 18:59:02 -0300

    agregamos la funcion validarCantidadCaracteres, resumenValidaciones

--- js/administrador.js
+++ js/administrador.js
@@ -1,6 +1,8 @@
 //MODULOS - Dividir en varios archivos js un mismo problema. El html no tiene acceso al archivo js.
 // 2) Traer la clase desde otro archivo js: import - cosa a importar - from - "ruta del archivo (como link archivos de css)"
 import Pelicula from "./classPelicula.js";
+import { resumenValidaciones } from "./helpers.js";
+
 
 //Variables globales
 let formularioPeliculas = document.getElementById("form-pelicula");
@@ -36,25 +38,31 @@ function prepararFormularioPelicula (e) {
 
 function crearPelicula() {
     // 1) validar los datos (validacion html y validacion js)
-
+    const resumen = resumenValidaciones(titulo.value);
     // 2) si los datos son validos, entonces crear pelicula
-    const peliculaEj = new Pelicula(
-        "001",
-        "El señor de los anillos: La comunidad del anillo",
-        "La Comunidad del Anillo es la primera parte de la trilogía El Señor de los Anillos, de J. R. R. Tolkien.",
-        "https://es.wikipedia.org/wiki/El_se%C3%B1or_de_los_anillos:_La_comunidad_del_anillo#/media/Archivo:The_Lord_of_the_Rings_-_The_Fellowship_of_the_Ring.jpg",
-        "Fantasía",
-        2001,
-        "2 horas y 58 minutos",
-        "Estados Unidos, Nueva Zelanda",
-        ["Elijah Wood", "Ian McKellen", "Viggo Mortensen", "Sean Astin", "Orlando Bloom", "John Rhys-Davies", "Dominic Monaghan", "Billy Boyd", "Sean Bean"]
-    );
+    if (resumen.length === 0){
+        const peliculaEj = new Pelicula(
+            "001",
+            "El señor de los anillos: La comunidad del anillo",
+            "La Comunidad del Anillo es la primera parte de la trilogía El Señor de los Anillos, de J. R. R. Tolkien.",
+            "https://es.wikipedia.org/wiki/El_se%C3%B1or_de_los_anillos:_La_comunidad_del_anillo#/media/Archivo:The_Lord_of_the_Rings_-_The_Fellowship_of_the_Ring.jpg",
+            "Fantasía",
+            2001,
+            "2 horas y 58 minutos",
+            "Estados Unidos, Nueva Zelanda",
+            ["Elijah Wood", "Ian McKellen", "Viggo Mortensen", "Sean Astin", "Orlando Bloom", "John Rhys-Davies", "Dominic Monaghan", "Billy Boyd", "Sean Bean"]
+        );
+    
+        // 3) guardar los datos en un array (variable global: listaPeliculas)
+    
+        // 4) guardar el array en el localstorage
+    
+        // 5) mensaje de error
+    
+        console.log(peliculaEj);
 
-    // 3) guardar los datos en un array (variable global: listaPeliculas)
+    } else {
+        console.log("Aqui hay errores");
+    }
 
-    // 4) guardar el array en el localstorage
-
-    // 5) mensaje de error
-
-    console.log(peliculaEj);
 }
