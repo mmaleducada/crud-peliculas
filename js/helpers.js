1f73ea1cce44f4039a5e8941868914ad1e941e72
Malena de Arriba <dearribamalena@gmail.com>
2023-05-01 20:35:47 -0300

    validacion del campo imagen

--- js/helpers.js
+++ js/helpers.js
@@ -10,11 +10,6 @@ function validarCantidadCaracteres(texto, min, max,) {
     }
 }
 
-//https://pics.filmaffinity.com/mavka_the_forest_song-447812217-mmed.jpg
-
-function validarURLImagen(imagen) {
-
-}
 
 export function resumenValidaciones(titulo, descripcion, imagen, genero, anio, duracion, pais, director, reparto) {
     let resumen = '';
@@ -22,35 +17,40 @@ export function resumenValidaciones(titulo, descripcion, imagen, genero, anio, d
         //si no cumplio la validacion
         resumen += "El titulo debe contener entre 2 y 100 caracteres <br>";
     };
-
+    
     if (!validarCantidadCaracteres(descripcion, 5, 300)) {
         //si no cumplio la validacion
         resumen += "La descripcion debe contener entre 5 y 300 caracteres <br>";
     };
 
+    if (!validarURLImagen(imagen)) {
+        //si no cumplio la validacion
+        resumen += "La URL debe ser valida y contener una extension (.jpg, .png o .gif) <br>";
+    };
+    
     if (!validarAño(anio, 1990)) {
         resumen += "El año de estreno debe ser entre 1990 y el proximo año <br>";
     }
-
+    
     if (!validarDuracion(duracion, 10, 241)) {
         resumen += "La duracion debe ser entre 10 min y 241 min"
     }
-
+    
     if (!validarCantidadCaracteres(pais, 4, 100)) {
         //si no cumplio la validacion
         resumen += "El pais debe contener entre 4 y 100 caracteres <br>";
     };
-
+    
     if (!validarCantidadCaracteres(director, 4, 100)) {
         //si no cumplio la validacion
         resumen += "El director debe contener entre 4 y 100 caracteres <br>";
     };
-
+    
     if (!validarCantidadCaracteres(reparto, 4, 300)) {
         //si no cumplio la validacion
         resumen += "El reparto debe contener entre 4 y 300 caracteres <br>";
     };
-
+    
     return resumen;
 }
 
@@ -65,12 +65,28 @@ function validarDuracion(tiempo, min, max) {
 
 function validarAño(anio, desde) {
     const añoActual = new Date().getFullYear(); // Obtener el año actual <= Math.min(hasta, añoActual)
-
+    
     if (parseInt(anio) >= desde && parseInt(anio) <= añoActual) {
         console.log("El año está dentro del rango válido")// El año está dentro del rango válido
         return true;
     } else {
         // El año no está dentro del rango válido
         console.log("El año no está dentro del rango válido")
+    }
+}
+
+//https://pics.filmaffinity.com/mavka_the_forest_song-447812217-mmed.jpg
+
+// ^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)$   ---> van siempre entre dos /expresion regular/
+
+function validarURLImagen(imagen) {
+    const patron = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)$/;
+    // .test: metodo que verifica si se cumple con la expresion regular. Devuelve TRUE o FALSE
+    if(patron.test(imagen)){
+        console.log("La url de la imagen es valida");
+        return true;
+    } else {
+        console.log("La url de la imagen es invalida");
+        return false;
     }
 }
